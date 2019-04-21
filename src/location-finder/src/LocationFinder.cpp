5998323df8a92477294d57a3d21b695143b41470
tttapa <pieter.pas@outlook.com>
2019-04-21 14:13:45 +0200

    Fix workdir Docker

--- src/location-finder/src/LocationFinder.cpp
+++ src/location-finder/src/LocationFinder.cpp
@@ -1,30 +1,58 @@
 #include <LocationFinder.hpp>
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <string>
 
 #include <opencv2/core/utils/logger.hpp>
 
 using namespace cv;
+using std::string;
+
+string type2str(int type) {
+    string r;
+
+    uchar depth = type & CV_MAT_DEPTH_MASK;
+    uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+    switch (depth) {
+        case CV_8U: r = "8U"; break;
+        case CV_8S: r = "8S"; break;
+        case CV_16U: r = "16U"; break;
+        case CV_16S: r = "16S"; break;
+        case CV_32S: r = "32S"; break;
+        case CV_32F: r = "32F"; break;
+        case CV_64F: r = "64F"; break;
+        default: r = "User"; break;
+    }
+
+    r += "C";
+    r += (chans + '0');
+
+    return r;
+}
 
 int test() {
     utils::logging::setLogLevel(utils::logging::LogLevel::LOG_LEVEL_VERBOSE);
     std::cout << "Trying to create capture" << std::endl;
     VideoCapture cap(0 + CAP_V4L2);  // open the default camera
     std::cout << "VideoCapture created" << std::endl;
-    cap.set(CAP_PROP_FRAME_WIDTH, 410);
-    cap.set(CAP_PROP_FRAME_HEIGHT, 308);
-    std::cout << "VideoCapture dimensions set" << std::endl;
+    // cap.set(CAP_PROP_FRAME_WIDTH, 410);
+    // cap.set(CAP_PROP_FRAME_HEIGHT, 308);
+    // std::cout << "VideoCapture dimensions set" << std::endl;
     if (!cap.isOpened())  // check if we succeeded
         return -1;
     std::cout << "VideoCapture is open" << std::endl;
     Mat frame;
     cap >> frame;  // get a new frame from camera
     std::cout << "VideoCapture captured" << std::endl;
-    Mat edges;
-    cvtColor(frame, edges, COLOR_BGR2GRAY);
-    GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
-    Canny(edges, edges, 0, 30, 3);
-    imwrite("test.bmp", edges);
+
+    string ty = type2str(M.type());
+    printf("Matrix: %s %dx%d, depth: %d\n", ty.c_str(), M.cols, M.rows, M.depth());
+
+    Mat blur;
+    GaussianBlur(frame, blur, Size(7, 7), 1.5, 1.5);
+    imwrite("test.bmp", frame);
+    imwrite("blur.bmp", blur);
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
 }
--- src/location-finder/test/test-LocationFinder.cpp
+++ src/location-finder/test/test-LocationFinder.cpp
@@ -1,4 +1,28 @@
 #include <LocationFinder.hpp>
 #include <gtest/gtest.h>
 
-TEST(DISABLED_TODO, TODO) { ASSERT_EQ(test(), 0); }
+TEST(OpenCV, test) { ASSERT_EQ(test(), 0); }
+
+#include <immintrin.h>
+
+TEST(AVX, compareGT) {
+    int8_t values[sizeof(__m256i)] = {
+        63, -64, 63, -64, //
+        63, -64, 63, -64, //
+        63, -64, 63, -64, //
+        63, -64, 63, -64, //
+        63, -64, 63, -64, //
+        63, -64, 63, -64, //
+        63, -64, 63, -64, //
+        63, -64, 63, -64, //
+    };
+    int8_t zeros[sizeof(__m256i)] = {};
+    __m256i a = _mm256_load_si256(reinterpret_cast<__m256i*>(values));
+    __m256i b=_mm256_load_si256(reinterpret_cast<__m256i*>(zeros));;
+    __m256i c = _mm256_cmpgt_epi8(a, b);
+
+    const int8_t *result = reinterpret_cast<const int8_t*>(&c);
+    for (size_t i = 0; i < sizeof(__m256i); ++i)
+        std::cout << +result[i] << ' ';
+    std::cout << std::endl;
+}
