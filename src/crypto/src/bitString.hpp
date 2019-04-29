a89057ec2c7ac5c97bf204ec313a398851c54a84
Pieter Pas <pieter.pas@outlook.com>
2019-04-29 18:19:14 +0200

    Add improved BitString::split(array) prototypes

--- src/crypto/src/bitString.hpp
+++ src/crypto/src/bitString.hpp
@@ -1,14 +1,17 @@
-#include <vector>
 #include <cstdint>
+#include <vector>
+#include <array>
 
 class BitString {
-    private:
+  private:
     std::vector<uint8_t> bits;
 
-    public:
+  public:
     BitString(std::vector<uint8_t> input);
 
     void concatenate(const BitString &otherBitString);
 
     void split(uint32_t array[]);
+    void split(uint32_t (&array)[10]);
+    void split(std::array<uint32_t, 10> &array);
 };
