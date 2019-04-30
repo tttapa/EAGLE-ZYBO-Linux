f662900da28b667438bedba286bf6ba19defdb6b
Pieter Pas <pieter.pas@outlook.com>
2019-04-30 14:10:14 +0200

    Add vision app + install-opencv script

---
+++ applications/vision/vision.cpp
@@ -0,0 +1,46 @@
+#include <ANSIColors.hpp>
+#include <LocationFinder.hpp>
+#include <LocationTracker.hpp>
+#include <PerfTimer.hpp>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <string>
+
+using namespace std;
+
+void loop();
+
+int main() {
+    while (1) {
+        try {
+            loop();
+        } catch (std::runtime_error &e) {
+            cerr << ANSIColors::redb << e.what() << ANSIColors::reset << endl;
+            cerr << "Restarting loop ..." << endl;
+        }
+    }
+}
+
+void loop() {
+    LocationFinder lf = cv::VideoCapture(0);
+    LocationTracker lt;
+
+    cv::VideoCapture &cap = lf.getCapture();
+    int frame_width       = cap.get(cv::CAP_PROP_FRAME_WIDTH);
+    int frame_height      = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+    double fps            = cap.get(cv::CAP_PROP_FPS);
+
+    cout << "Frame dimensions: (" << frame_width << "×" << frame_height << ")\n"
+         << "Frame rate      : " << fps << " fps" << std::endl;
+
+    cv::Mat locimg;
+    while (true) {
+        PerfTimer pt;
+        Point locInSquare = lf.updateLocation();
+        Point location    = lt.update(locInSquare);
+        auto duration     = pt.getDuration();
+        cout << "Position: " << location << endl;
+        cout << "Vision duration: " << duration << " µs → " << 1e6 / duration
+             << " fps" << endl;
+    }
+}
