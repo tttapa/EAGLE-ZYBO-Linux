55b327c825c05fbdd98263c5c1655c75bd958d5b
Pieter Pas <pieter.pas@outlook.com>
2019-05-19 22:26:39 +0200

    toby logger dict code

--- applications/logger-offsets/main.cpp
+++ applications/logger-offsets/main.cpp
@@ -4,6 +4,6 @@
 
 int main() {
     LogEntry l = {};
-    std::cout << "Offset = " << +((uintptr_t) &l.altitudeControlSignal - (uintptr_t) &l)
+    std::cout << "Offset = " << +((uintptr_t) &l.rcInput.throttle - (uintptr_t) &l)
               << " bytes" << std::endl;
 }
--- python-logentry/loggerToby.py
+++ python-logentry/loggerToby.py
@@ -2,4 +2,13 @@ import struct
 
 def loggerToby(b, dict):
     for x in dict:
-        pass
+        if x == "flightMode":
+            dict[x] = struct.unpack('i', b[305:309])[0]
+        if x == "destination":
+            dict[x] = struct.unpack('ff', b[160:168]) + struct.unpack('f', b[12:16])
+        if x == "currentPosition":
+            dict[x] = struct.unpack('ff', b[176:184]) + struct.unpack('f', b[20:24])
+        if x == "currentOrientation":
+            dict[x] = struct.unpack('ffff', b[96:112])
+        if x == "thrust":
+            dict[x] = struct.unpack('f', b[245:249])[0]
