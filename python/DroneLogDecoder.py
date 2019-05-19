d030ace361c30293f49f66fc483730d1ed4379de
Pieter Pas <pieter.pas@outlook.com>
2019-05-19 12:37:55 +0200

    DroneLogDecoder.py

--- python/DroneLogDecoder.py
+++ python/DroneLogDecoder.py
@@ -1,9 +1,13 @@
-from DroneLogger import *
+from DroneLogger import LogEntry
 import os
 
 logfile = "eagle-2019-05-18.18.54.53.dat"
-with open(os.path.join(getenv("HOME"), "dronelogs", logfile), 'rb') as f:
+with open(os.path.join(os.getenv("HOME"), "dronelogs", logfile), 'rb') as f:
     logentries = f.read()
-    if len(logentries) % len(bytes(LogEntry())) != 0:
-        raise RuntimeError
-    
+    size = len(bytes(LogEntry()))
+    if len(logentries) % size != 0:
+        raise RuntimeError(
+            "Error: file size is not a multiple of the log entry size")
+    for i in range(len(logentries) // size):
+        logentry = LogEntry(logentries[size * i:size * (i+1)])
+        print(logentry.millis)
